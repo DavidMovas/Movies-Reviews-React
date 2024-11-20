aa3b14b4a50e8c7ca238011b0252db0b5297a90f
Nofre <davidprogram417@gmail.com>
2024-11-20 22:45:06 +0100

    created route config

--- src/app/providers/Router/ui/AppRouter.tsx
+++ src/app/providers/Router/ui/AppRouter.tsx
@@ -1,14 +1,22 @@
 import React, { Suspense } from 'react';
 import { Route, Routes } from "react-router-dom";
-import { MainPage } from "pages/main";
-import { AboutPage } from "pages/about";
+import { routeConfig } from "shared/config/routeConfig/routeConfig";
 
 const AppRouter = () => {
     return (
         <Suspense fallback={<div>Loading...</div>}>
             <Routes>
-                <Route path="/" element={<MainPage/>}/>
-                <Route path="/about" element={<AboutPage/>}/>
+                {Object.values(routeConfig).map(({element, path}) => (
+                    <Route
+                        key={path}
+                        path={path}
+                        element={(
+                            <div className="page-wrapper">
+                                {element}
+                            </div>
+                        )}
+                    />
+                ))}
             </Routes>
         </Suspense>
     );
