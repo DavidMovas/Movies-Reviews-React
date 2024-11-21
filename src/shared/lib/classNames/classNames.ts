d10653cb72bcdb2ec8d7e7fc8bb33ff85bbc0821
Nofre <davidprogram417@gmail.com>
2024-11-21 17:34:30 +0100

    added babel config file, first unit tests were did write

--- src/shared/lib/classNames/classNames.ts
+++ src/shared/lib/classNames/classNames.ts
@@ -8,8 +8,9 @@ export function classNames(cls: string, mods: Mods = {}, additional?: (string | 
     return [
         cls,
         ...additional.filter(Boolean) ,
-        Object.entries(mods)
-            .filter(([value]) => Boolean(value))
+        ...Object.entries(mods)
+            // eslint-disable-next-line @typescript-eslint/no-unused-vars
+            .filter(([_, value]) => Boolean(value))
             .map(([cls]) => cls)
     ]
         .join(' ');
