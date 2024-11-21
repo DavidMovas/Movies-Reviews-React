1ed8e71b945d0d2c66b836ce9f216d2fc6fae913
Nofre <davidprogram417@gmail.com>
2024-11-21 20:04:37 +0100

    added plugin for analize bundle size

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -3,6 +3,7 @@ import webpack from "webpack";
 import { BuildPaths } from "./types/config";
 import MiniCssExtractPlugin from "mini-css-extract-plugin";
 import { BuildOptions } from "./types/config";
+import {BundleAnalyzerPlugin } from "webpack-bundle-analyzer";
 
 export function buildPlugins(options: BuildOptions, {html}: BuildPaths): webpack.WebpackPluginInstance[] {
     return [
@@ -18,5 +19,8 @@ export function buildPlugins(options: BuildOptions, {html}: BuildPaths): webpack
             __IS_DEV__: JSON.stringify(options.isDev),
         }),
         new webpack.HotModuleReplacementPlugin(),
+        new BundleAnalyzerPlugin({
+            openAnalyzer: false,
+        }),
     ]
 }
