1abb0380ff73850b798eb382bf1550942c1f5b04
Nofre <davidprogram417@gmail.com>
2024-11-27 00:53:48 +0100

    Theme Provider adjusted

--- src/app/providers/ThemeProvider/ui/ThemeProvider.tsx
+++ src/app/providers/ThemeProvider/ui/ThemeProvider.tsx
@@ -3,10 +3,20 @@ import React, { FC, useMemo } from "react";
 
 const defaultTheme = localStorage.getItem(LOCAL_STORAGE_THEME_KEY) as Theme || Theme.LIGHT;
 
-const ThemeProvider: FC<{ children: React.ReactNode }> = ({children}) => {
-    const [theme, setTheme] = React.useState<Theme>(defaultTheme);
+interface ThemeProviderProps {
+    initialTheme?: Theme;
+    children?: React.ReactNode;
+}
 
-    const defaultProps = useMemo(() => ({
+const ThemeProvider: FC<ThemeProviderProps> = (props) => {
+   const {
+       initialTheme,
+       children
+   } = props;
+
+    const [theme, setTheme] = React.useState<Theme>(defaultTheme || initialTheme);
+
+    const defaultProps =  useMemo(() => ({
         theme: theme,
         setTheme: setTheme
     }), [theme, setTheme]);
