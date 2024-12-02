ca7e2f5a898fca662fe00b5423f6d92171de1343
Nofre <davidprogram417@gmail.com>
2024-12-02 19:26:50 +0100

    LoginModal component from Main page removed, ru translation adjusted

--- src/pages/main/ui/Main.tsx
+++ src/pages/main/ui/Main.tsx
@@ -1,25 +1,12 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useTranslation } from "react-i18next";
-import { Input } from "shared/ui/Input/Input";
 
 function Main() {
     const {t} = useTranslation();
 
-    const [value, setValue] = useState('');
-
-    const onChange = (val: string) => {
-        setValue(val);
-    }
-
     return (
         <div>
             <h1>{t('Main')}</h1>
-            <Input
-                label={"Enter value"}
-                placeholder={"Type here"}
-                type="text"
-                value={value}
-                onChange={onChange}></Input>
         </div>
     );
 }
