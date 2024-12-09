ac6e4347d5b487698e690884fc60f2d5857da62d
Nofre <davidprogram417@gmail.com>
2024-12-09 19:14:56 +0100

    added Review component

--- src/enteties/Movie/ui/MovieDetails/MovieReviewItem/MovieReviewItem.tsx
+++ src/enteties/Movie/ui/MovieDetails/MovieReviewItem/MovieReviewItem.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { classNames } from "shared/lib/classNames/classNames";
-import * as cls from "./MovieReviewItem.module.css"
 import { Review } from "enteties/Review";
 import { ContentPanel } from "shared/ui/ContentPanel/ContentPanel";
 import { Text, TextSize, TextTheme } from "shared/ui/Text/Text";
 import { Rating } from "shared/ui/Rating/Rating";
+import * as cls from "./MovieReviewItem.module.css"
 
 interface MovieReviewItemProps {
     className?: string;
@@ -21,10 +21,13 @@ export const MovieReviewItem = (props: MovieReviewItemProps) => {
         <div
             className={classNames(cls.MovieReviewItem, {}, [className])}
         >
-            <ContentPanel>
+            <ContentPanel className={cls.review}>
                 <div className={cls.header}>
                     <Text title={review.title} theme={TextTheme.INVERTED} size={TextSize.SMALL}/>
                     <Rating value={review.rating} theme={TextTheme.INVERTED}/>
+                </div>
+                <div className={cls.content}>
+                    <Text text={review.description} theme={TextTheme.INVERTED} size={TextSize.SMALL_S}/>
                 </div>
             </ContentPanel>
         </div>
--- src/enteties/Movie/ui/MovieDetails/MovieReviewPanel/MovieReviewPanel.tsx
+++ src/enteties/Movie/ui/MovieDetails/MovieReviewPanel/MovieReviewPanel.tsx
@@ -3,10 +3,10 @@ import { classNames } from "shared/lib/classNames/classNames";
 import { ContentPanel } from "shared/ui/ContentPanel/ContentPanel";
 import { Text, TextSize } from "shared/ui/Text/Text";
 import { useTranslation } from "react-i18next";
-import * as cls from "./MovieReviewPanel.module.css"
 import { MovieReviewItem } from "enteties/Movie/ui/MovieDetails/MovieReviewItem/MovieReviewItem";
 import { Review } from "enteties/Review";
 import { formatDateString } from "shared/lib/date/formatDateString";
+import * as cls from "./MovieReviewPanel.module.css"
 
 interface MovieReviewPanelProps {
     className?: string;
@@ -26,7 +26,7 @@ export const MovieReviewPanel = (props: MovieReviewPanelProps) => {
         userId: 21,
         movieId: 501,
         title: "Title to great movie",
-        description: "Great movie ! I like this movie !",
+        description: "A cyborg, identical to the one who failed to kill Sarah Connor, must now protect her ten year old son John from an even more advanced and powerful cyborg. A cyborg, identical to the one who failed to kill Sarah Connor, must now protect her ten year old son John from an even more advanced and powerful cyborg. A cyborg, identical to the one who failed to kill Sarah Connor, must now protect her ten year old son John from an even more advanced and powerful cyborg.",
         rating: 8,
         createdAt: formatDateString("2015-01-01"),
     }
@@ -34,12 +34,12 @@ export const MovieReviewPanel = (props: MovieReviewPanelProps) => {
     // make here fetching all reviews by movie ID
 
     return (
-        <div
-            className={classNames(cls.MovieReviewPanel, {}, [className])}
-        >
-            <ContentPanel>
+        <div className={classNames(cls.MovieReviewPanel, {}, [className])}>
+            <ContentPanel className={cls.block}>
                 <Text className={cls.title} title={t("Reviews")} size={TextSize.SMALL}/>
-                <MovieReviewItem review={testReview}/>
+                <div className={cls.reviews}>
+                    <MovieReviewItem review={testReview}/>
+                </div>
             </ContentPanel>
         </div>
     );
